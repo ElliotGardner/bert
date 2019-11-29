9b8859767f403a6f8e82afd7ebbd66bb1be297f5
Elliot <40443985+ElliotGardner@users.noreply.github.com>
2019-11-29 15:05:56 -0600

    Changed Cola classifier for my purposes

--- run_classifier.py
+++ run_classifier.py
@@ -313,7 +313,7 @@ class MrpcProcessor(DataProcessor):
 
   def get_labels(self):
     """See base class."""
-    return ["0", "1"]
+    return ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
 
   def _create_examples(self, lines, set_type):
     """Creates examples for the training and dev sets."""
